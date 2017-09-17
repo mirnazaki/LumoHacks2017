d3e8c8843bef316a6639bd12c57cdeea1a826b46
Matilda Chiang <chiang.matilda@gmail.com>
2017-09-16 17:13:20 -0700

    added stats button and added text to buttons

---
+++ IntheClouds/app/src/main/java/com/example/mirna/intheclouds/StatsInfo.java
@@ -0,0 +1,17 @@
+package com.example.mirna.intheclouds;
+
+import android.os.Bundle;
+
+/**
+ * Created by cubefreak716 on 2017-09-16.
+ */
+
+public class StatsInfo extends MainActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statsinfo);
+
+
+    }
+
+}
